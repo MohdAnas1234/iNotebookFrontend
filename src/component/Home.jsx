45f6afa66790d3afbb398ffcc54f7d4c9e060185
MohdAnas1234 <mohdanas2403@gmail.com>
2025-12-05 23:50:03 +0530

    first commit~

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -15,22 +15,3 @@ const Home = (props) => {
 export default Home
 
 
-// import React from "react";
-// import { Navigate } from "react-router-dom";
-// import Notes from "./Notes";
-
-// const Home = ({ showAlert }) => {
-  
-//   // Block this page if token does not exist
-//   if (!localStorage.getItem("token")) {
-//     return <Navigate to="/login" replace />;
-//   }
-
-//   return (
-//     <div>
-//       <Notes showAlert={showAlert} />
-//     </div>
-//   );
-// };
-
-// export default Home;
